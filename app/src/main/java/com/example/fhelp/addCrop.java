8e9703745ed608c4df72de3f87f5a202b8d71860
Dibyanshu Mishra <dibyenshu@gmail.com>
2020-05-17 23:33:16 +0530

    addCrop function working.

--- app/src/main/java/com/example/fhelp/addCrop.java
+++ app/src/main/java/com/example/fhelp/addCrop.java
@@ -6,9 +6,12 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import org.json.JSONException;
@@ -19,25 +22,32 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class addCrop extends AppCompatActivity {
 
-    EditText editText_cropName;
+    RadioGroup radioGroup;
+    RadioButton radioButton;
     Button button_addCrop;
     String urlString;
+    ArrayList<String> crops;
     URL url;
-    JSONObject jsonObject;
+    JSONObject jsonObject,detailsJsonObject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_crop);
-        editText_cropName = findViewById(R.id.cropName);
+
         button_addCrop = findViewById(R.id.addCrop);
+        radioGroup = findViewById(R.id.cropRadioGroup);
+        crops = new ArrayList<String>();
+        populateCrops();
+
+
         SharedPreferences preferences = this.getSharedPreferences("farming_assistant", Context.MODE_PRIVATE);
         urlString = preferences.getString("url", "not found");
         int sessionId = preferences.getInt("sessionId", -1);
-
         urlString = urlString + "addCrop/" + String.valueOf(sessionId);
 
         try {
@@ -50,22 +60,22 @@ public class addCrop extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 jsonObject = new JSONObject();
-                if(editText_cropName.getText().toString().toLowerCase().equals("rice")){
-                    try {
-                        jsonObject.put(editText_cropName.getText().toString().toLowerCase(),"1");
-                        jsonObject.put("wheat","0");
-                    } catch (JSONException e) {
-                        e.printStackTrace();
+                detailsJsonObject = new JSONObject();
+                int radioButtonId = radioGroup.getCheckedRadioButtonId();
+                radioButton = findViewById(radioButtonId);
+                //logic to build up the json object
+                try{
+                    for(int i=0;i<crops.size();i++){
+                        if(radioButton.getText().toString().equals(crops.get(i))) jsonObject.put(crops.get(i),"1");
+                        else jsonObject.put(crops.get(i),"0");
                     }
+                    getDetails();
+                    jsonObject.put("details",detailsJsonObject);
+                }catch(Exception ex){
+                    Log.e("error",ex.toString());
                 }
-                else{
-                    try {
-                        jsonObject.put(editText_cropName.getText().toString().toLowerCase(),"1");
-                        jsonObject.put("rice","0");
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
+
+                //jsonObject formed
                 new AddCropPost().execute(url);
             }
         });
@@ -100,6 +110,30 @@ public class addCrop extends AppCompatActivity {
                 e.printStackTrace();
             }
             return null;
+        }
+    }
+
+    private void populateCrops(){
+        crops.add(getResources().getString(R.string.crop_rice));
+        crops.add(getResources().getString(R.string.crop_wheat));
+        crops.add(getResources().getString(R.string.crop_mungBean));
+        crops.add(getResources().getString(R.string.crop_millet));
+        crops.add(getResources().getString(R.string.crop_tea));
+        crops.add(getResources().getString(R.string.crop_maize));
+    }
+
+    private void getDetails(){
+        try {
+            detailsJsonObject.put(getResources().getString(R.string.details_precipitation),((EditText)findViewById(R.id.precipitationEditText)).getText().toString());
+            detailsJsonObject.put(getResources().getString(R.string.details_temp),((EditText)findViewById(R.id.tempEditText)).getText().toString());
+            detailsJsonObject.put(getResources().getString(R.string.details_humidity),((EditText)findViewById(R.id.humidityEditText)).getText().toString());
+            detailsJsonObject.put(getResources().getString(R.string.details_moisture),((EditText)findViewById(R.id.moistureEditText)).getText().toString());
+            detailsJsonObject.put(getResources().getString(R.string.details_soilTemp),((EditText)findViewById(R.id.soilTempEditText)).getText().toString());
+            detailsJsonObject.put(getResources().getString(R.string.details_ph),((EditText)findViewById(R.id.phEditText)).getText().toString());
+            detailsJsonObject.put(getResources().getString(R.string.details_waterPh),((EditText)findViewById(R.id.waterPhEditText)).getText().toString());
+
+        }catch(Exception ex){
+            Log.e("error",ex.toString());
         }
     }
 }
